5ba40d0cc241a1c2701c17b3949dc18ebd156f19
Jonathon Klem <jonathonklem@gmail.com>
2015-01-10 18:04:18 -0600

    Update quiz.php
    Improved formatting and organization

--- quiz.php
+++ quiz.php
@@ -4,11 +4,44 @@
 	 *   Add command line options for controlling:
 	 *     1) Number of questions
 	 *     2) Order of questions
-   */
+         */
 
 
-// functions
+	// get command line options
+	$options = getopt("cnf:d:");
+	$quiz = $options['f'];	
 
+	// check if delimiter is set. Default to ","
+	if (isset($options['d'])) {
+		$delimiter = $options['d'];
+	} else {
+		$delimiter = ",";
+	}
+
+	// end with usage statement if file is not specified
+	if ($argc < 3) {
+		die("Usage: {$argv[0]} -f path/to/quiz.txt {-n}\n");
+	}
+
+	// end if file cannot be opened
+	if (($file = @fopen($quiz, "r")) === FALSE) {
+		die("Could not open $quiz for reading\n");
+	}
+
+	// run quiz
+	$quiz_data = getData($file, $delimiter);
+
+	// if -c is not set (flash card mode) run in default.
+	if (!isset($options['c'])) {	
+		quizMode($quiz_data, $options);
+	// if -c is set, run in flash card mode	
+	} elseif (isset($options['c'])) {
+		flashMode($quiz_data);
+	}
+
+	fclose($file);
+
+	// functions
 	function getData($file, $delimiter) {
 		$cnt = 0;
 		while (($data = fgetcsv($file, 1000, $delimiter)) !== FALSE) {
@@ -92,50 +125,3 @@
 			system("clear");
 		}
 	}
-
-
-
-
-
-// get command line options
-	
-	$options = getopt("cnf:d:");
-	$quiz = $options['f'];	
-
-// check if delimiter is set. Default to ","
-
-	if (isset($options['d'])) {
-		$delimiter = $options['d'];
-	} else {
-		$delimiter = ",";
-	}
-
-// end with usage statement if file is not specified
-
-	if ($argc < 3) {
-		die("Usage: {$argv[0]} -f path/to/quiz.txt {-n}\n");
-	}
-
-// end if file cannot be opened
-
-	if (($file = @fopen($quiz, "r")) === FALSE) {
-		die("Could not open $quiz for reading\n");
-	}
-
-
-
-
-// run quiz
-	$quiz_data = getData($file, $delimiter);
-
-	// if -c is not set (flash card mode) run in default.
-	if (!isset($options['c'])) {	
-		quizMode($quiz_data, $options);
-
-	// if -c is set, run in flash card mode	
-	} elseif (isset($options['c'])) {
-
-		flashMode($quiz_data);
-		}
-
-	fclose($file);
